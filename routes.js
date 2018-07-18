6193046345279b93e59fb990b33df932acddf943
ras user <ras@test.com>
2018-07-18 12:55:21 +0530

    wip

---
+++ routes.js
@@ -0,0 +1,43 @@
+const RasLog = require('./models/ras-log');
+
+module.exports = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+    
+            return 'Hello, world!';
+        }
+    },
+    
+    {
+        method: 'GET',
+        path: '/raslog',
+        handler: function (request, h) {
+            return RasLog.find({});
+        }
+    },
+    {
+        method: ['PUT', 'POST'],
+        path: '/raslog',
+        handler: async (request, h) => { 
+            const params = request.query;   
+            const raslog = new RasLog({
+                "mobile": params.mobile,
+                "serviceId": params.serviceId,
+                "departmentId": params.departmentId,
+                "stateId": params.stateId,
+                "registration": "",
+                "email": "",
+                "datetime": new Date(),
+                "ip": "",
+                "is_error": "",
+                "is_code": "",
+                "error_message": "",
+            });
+            const savedData = await raslog.save(raslog);
+            return h.response(savedData)
+        }
+    }
+
+];
